2c57814f6aeb78721d8feb15e7bfb771ac66c560
Kaleshe <kaleshe@neuralalpha.com>
2024-12-15 17:00:45 +0000

    feat: protect applications route and create page

--- src/restful/routes/applicationsRouter.js
+++ src/restful/routes/applicationsRouter.js
@@ -4,23 +4,41 @@ const {
 } = require("../controllers/applicationsController");
 const { protect } = require("../../middleware");
 const checkSubscribedUser = require("../../helper/checkSubscribedUser");
+const allowedRole = require("../../helper/allowedRole");
 
 const router = Router();
 
 // GET all applications
-router.get("/", ApplicationsController.getAllApplications);
+router.get(
+  "/",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
+  ApplicationsController.getAllApplications,
+);
 
 // GET applications by opportunity ID
 router.get(
   "/opportunity/:opportunityId",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
   ApplicationsController.getAllApplicationsById,
 );
 
 // GET applications by user ID
-router.get("/user/:userId", ApplicationsController.getApplicationsByUserId);
+router.get(
+  "/user/:userId",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
+  ApplicationsController.getApplicationsByUserId,
+);
 
 // GET application by ID
-router.get("/:applicationId", ApplicationsController.getApplicationById);
+router.get(
+  "/:applicationId",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
+  ApplicationsController.getApplicationById,
+);
 
 // POST a new application
 router.post(
@@ -31,9 +49,19 @@ router.post(
 );
 
 // PATCH update an existing application
-router.patch("/:applicationId", ApplicationsController.updateApplication);
+router.patch(
+  "/:applicationId",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
+  ApplicationsController.updateApplication,
+);
 
 // DELETE an application
-router.delete("/:applicationId", ApplicationsController.deleteApplication);
+router.delete(
+  "/:applicationId",
+  protect,
+  allowedRole(["admin", "super_admin", "brand"]),
+  ApplicationsController.deleteApplication,
+);
 
 module.exports.applicationsRouter = router;
